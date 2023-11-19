c4659ebd5ec57b3d09a8174f3c81fcfbcf3d5ecf
Ahmed Toba <ahmedtoba94@gmail.com>
2023-11-19 14:58:31 +0200

    adding cors

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
+
+const PORT = process.env.PORT || 5000;
 
 app.use('/api', createProxyMiddleware({
   target: 'https://api.stlouisfed.org',
   changeOrigin: true,
-  pathRewrite: {
-    '^/api': '',
-  },
+  pathRewrite: { '^/api': '' },
 }));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
